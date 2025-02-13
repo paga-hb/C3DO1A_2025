ebb19cfb863be772a5ce9618c41d4c99ba35fdee
Patrick <patrick.gabrielsson@hb.se>
2025-02-13 17:15:14 +0100

    add workshop 6

---
+++ workshop6/02_backing_services/fixtures/minio_initialize_and_seed.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Wait for Minio to become available
+while [ ! $(mc alias set minio http://minio:9000 minio_user minio_password) ]
+# while [ ! $(curl -I http://minio:9000/minio/health/live) ]
+do
+  echo 'Waiting for minio to start up ...'
+  sleep 0.1
+done
+sleep 5
+
+# Create public bucket with object
+/usr/bin/mc alias set minio http://minio:9000 minio_user minio_password
+/usr/bin/mc mb minio/videos
+/usr/bin/mc anonymous set public minio/videos
+/usr/bin/mc put /tmp/videos/SampleVideo_1280x720_1mb.mp4 minio/videos
+
+# Create user with readwrite and deleteobject access
+/usr/bin/mc admin user add minio Y42xk0CJlYV2fnShCtBP kCA01XftotivPbU9TRJ5Nrr5VMjFZNEhQhtvfWyY
+/usr/bin/mc admin policy attach minio readwrite --user Y42xk0CJlYV2fnShCtBP
+
+# Exit shell (so that container terminates)
+exit 0
